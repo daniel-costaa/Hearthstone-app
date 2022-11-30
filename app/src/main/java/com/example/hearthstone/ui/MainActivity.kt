22143c1182e0a07cb5f5c62ddb2e962800cde4e4
Daniel Costa <danielrezcosta97@gmail.com>
2022-11-30 20:09:42 -0300

    Created UI package

--- app/src/main/java/com/example/hearthstone/ui/MainActivity.kt
+++ app/src/main/java/com/example/hearthstone/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hearthstone
+package com.example.hearthstone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hearthstone.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
